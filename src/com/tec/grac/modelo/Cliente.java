c4af470d715f34a4b7449457d37e6db725585ab2
David Valverde Garro <dval7788@gmail.com>
2018-04-23 13:32:07 -0600

    * Agregados contructores y metodos accesores a Cliente y Vehiculo.

--- src/com/tec/grac/modelo/Cliente.java
+++ src/com/tec/grac/modelo/Cliente.java
@@ -9,4 +9,48 @@ public class Cliente {
     private String nombre;
     private String apellidos;
     private String direccion;
+
+    public Cliente(int idCliente, String nombre, String apellidos, String direccion) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellidos + " ; ID: " + idCliente;
+    }
 }
--- src/com/tec/grac/modelo/Vehiculo.java
+++ src/com/tec/grac/modelo/Vehiculo.java
@@ -10,4 +10,54 @@ public class Vehiculo {
     private int anno;
     private String color;
     private int asientos;
+
+    public Vehiculo(String placa, String modelo, int anno, String color, int asientos) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.anno = anno;
+        this.color = color;
+        this.asientos = asientos;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAsientos() {
+        return asientos;
+    }
+
+    public void setAsientos(int asientos) {
+        this.asientos = asientos;
+    }
+
+    //TODO Insertar toString()
 }
